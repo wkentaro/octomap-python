36e20aa710ddbc64dce537c50144e0a006d14fd1
nekanat <nekanat.stock@gmail.com>
2014-02-17 01:04:13 +0900

    Modify setup.py and version up

--- README.rst
+++ README.rst
@@ -13,9 +13,9 @@ and --library-dirs command line options:
 
     $ python setup.py build_ext --include-dirs /path/to/includes --library-dirs /path/to/libraries
 
-If you use ubuntu 12.04 and install ros/hydro, you can use the following command:
+If you use ubuntu 12.04 and install ros at /opt directory, you can use the following command:
 
-    $ python setup.py build_ext --include-dirs /opt/ros/hydro/include --library-dirs /opt/ros/hydro/lib
+    $ python setup.py build
 
 
 Install
--- setup.py
+++ setup.py
@@ -1,15 +1,42 @@
-
+import os
+import sys
 from distutils.core import Extension, setup
 from Cython.Distutils import build_ext
 
+platform_supported = False
+for prefix in ['darwin', 'linux', 'bsd']:
+    if prefix in sys.platform:
+        platform_supported = True
+        include_dirs = [
+            '/usr/include',
+            '/usr/local/include',
+        ]
+        lib_dirs = [
+            '/usr/lib',
+            '/usr/local/lib',
+        ]
+        if 'CPATH' in os.environ:
+            include_dirs += os.environ['CPATH'].split(':')
+        if 'LD_LIBRARY_PATH' in os.environ:
+            lib_dirs += os.environ['LD_LIBRARY_PATH'].split(':')
+        break
+
+if sys.platform == "win32":
+    platform_supported = False
+
+if not platform_supported:
+    raise NotImplementedError(sys.platform)
+
 setup(
     name="octomap",
-    version="0.1",
+    version="0.2",
     license = "BSD",
     packages=["octomap"],
     ext_modules=[Extension(
         "octomap",
         ["octomap/octomap.pyx"],
+        include_dirs = include_dirs,
+        library_dirs = lib_dirs,
         libraries=[
                 "octomap",
                 "octomath"
