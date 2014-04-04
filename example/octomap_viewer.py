af345c9b150d8921189f07de0ea1a0ac7e66f7ba
nekanat <nekanat.stock@gmail.com>
2014-04-04 22:06:47 +0900

    Bug fix

--- example/octomap_viewer.py
+++ example/octomap_viewer.py
@@ -12,24 +12,22 @@ class OctomapViewer:
         itr = self.tree.begin_tree()
         op = []
         fp = []
-        so = []
-        sf = []
         for i in itr:
             if i.isLeaf():
                 if self.tree.isNodeOccupied(i):
-                    so.append(i.getSize())
+                    so = i.getSize()
                     op.append(i.getCoordinate())
                 else:
                     if self.view_free:
-                        sf.append(i.getSize())
+                        sf = i.getSize()
                         fp.append(i.getCoordinate())
         op = zip(*op)
         fp = zip(*fp)
-        points3d(op[0], op[1], op[2], so, opacity=1.0, mode='cube',
-                 color=(0, 0, 1), scale_mode='none', scale_factor=0.1)
+        points3d(op[0], op[1], op[2], opacity=1.0, mode='cube',
+                 color=(0, 0, 1), scale_mode='none', scale_factor=so)
         if self.view_free:
-            points3d(fp[0], fp[1], fp[2], sf, opacity=0.3, mode='cube',
-                     color=(0, 1, 0), scale_mode='none', scale_factor=0.1)
+            points3d(fp[0], fp[1], fp[2], opacity=0.3, mode='cube',
+                     color=(0, 1, 0), scale_mode='none', scale_factor=sf)
         show()
 
 if __name__ == "__main__":
@@ -40,5 +38,6 @@ if __name__ == "__main__":
     parser.add_argument("-s", action="store_true",
                         default=False, dest="free", help="Draw free space.")
     options = parser.parse_args()
+
     viewer = OctomapViewer(options.filename, options.free)
     viewer.draw()
