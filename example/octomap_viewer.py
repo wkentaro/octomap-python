b2e563f6d9dfe718554aacd27525e341a2c3beb4
neka-nat <nekanat.stock@gmail.com>
2014-12-14 23:24:51 +0900

    Bug fix octomap_viewer.py

--- example/octomap_viewer.py
+++ example/octomap_viewer.py
@@ -10,24 +10,27 @@ class OctomapViewer:
         self.view_free = view_free
     def draw(self):
         itr = self.tree.begin_tree()
+        root_size = itr.getSize()
         op = []
         fp = []
+        so = []
+        sf = []
         for i in itr:
             if i.isLeaf():
                 if self.tree.isNodeOccupied(i):
-                    so = i.getSize()
+                    so.append(i.getSize() / root_size)
                     op.append(i.getCoordinate())
                 else:
                     if self.view_free:
-                        sf = i.getSize()
+                        sf.append(i.getSize() / root_size)
                         fp.append(i.getCoordinate())
         op = zip(*op)
         fp = zip(*fp)
-        points3d(op[0], op[1], op[2], opacity=1.0, mode='cube',
-                 color=(0, 0, 1), scale_mode='none', scale_factor=so)
+        points3d(op[0], op[1], op[2], so, opacity=1.0, mode='cube',
+                 color=(0, 0, 1), scale_mode='scalar', scale_factor=root_size)
         if self.view_free:
-            points3d(fp[0], fp[1], fp[2], opacity=0.3, mode='cube',
-                     color=(0, 1, 0), scale_mode='none', scale_factor=sf)
+            points3d(fp[0], fp[1], fp[2], sf, opacity=0.3, mode='cube',
+                     color=(0, 1, 0), scale_mode='scalar', scale_factor=root_size)
         show()
 
 if __name__ == "__main__":
