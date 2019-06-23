71a334abe8bee9f3fc6a377115fb574e513f0671
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-06-23 19:22:12 +0100

    1.8.0.post10

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ def main():
 
     skbuild.setup(
         name='octomap-python',
-        version='1.8.0.post9',
+        version='1.8.0.post10',
         install_requires=['numpy'],
         extras_require={
             'example': ['glooey', 'imgviz', 'pyglet', 'trimesh[easy]'],
